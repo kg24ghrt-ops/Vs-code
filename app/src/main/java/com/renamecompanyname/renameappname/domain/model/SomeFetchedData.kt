c4d58470fae290c760c5fecfa454c7ba46a52465
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-03-27 20:39:09 +0630

    Version 1 rewrite – project generator app

--- app/src/main/java/com/renamecompanyname/renameappname/domain/model/SomeFetchedData.kt
+++
@@ -1,6 +0,0 @@
-package com.renamecompanyname.renameappname.domain.model
-
-data class SomeFetchedData(
-    val name: String,
-    val flagUrl32: String,
-)
--- app/src/main/java/com/renamecompanyname/renameappname/domain/model/User.kt
+++
@@ -1,6 +0,0 @@
-package com.renamecompanyname.renameappname.domain.model
-
-data class User(
-    val id: String = "",
-    val name: String,
-)
