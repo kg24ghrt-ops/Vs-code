964fdeca3f7e63f0087de826d68eeae6c85cbe09
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-03-28 07:37:30 +0630

    Added download logic and template generator

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.renamecompanyname.renameappname"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.0"
+        versionCode = 2
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
